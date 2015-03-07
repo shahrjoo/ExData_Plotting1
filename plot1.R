ca5cc9992abe6202b29ece61c600e060e923daba
Ali Shahrjoo <ali.shahrjoo@gmail.com>
2015-03-08 03:27:17 +0330

    commit 1

---
+++ plot1.R
@@ -0,0 +1,16 @@
+#load the library
+library(data.table)
+
+#reading data from file
+df<-read.table("household_power_consumption.txt",sep=";", na.strings = "?",col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),header = TRUE)
+df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
+
+#subsetting the data
+df1<-subset(df,(df$Date>="2007-02-01" & df$Date<="2007-02-02"))
+
+#drawing histogram
+hist(df1$Global_active_power,col="red",main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+#create the png file
+dev.copy(png,file="plot1.png")
+dev.off()
